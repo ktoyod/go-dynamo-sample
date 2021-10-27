a6e6483c8192ffa2ee2b62a710db9b658db85b46
ktoyod <ytdh7834@gmail.com>
2021-10-27 10:55:49 +0900

    パスパラメータに対するレスポンス

--- app/main.go
+++ app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +11,13 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/user/:uid", func(c *gin.Context) {
+		uid := c.Param("uid")
+		// TODO: dynamoからid = uidのユーザ名を取得する
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("Hello %s", uid),
 		})
 	})
 	r.Run()
